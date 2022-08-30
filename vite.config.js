55ba4ed359c1ac4c624fb20858c79f3bcea9ac59
Daniel Ashby <dashby91@gmail.com>
2022-08-30 17:07:45 +1200

    chunk size limit modified

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
   plugins: [
     react({
       fastRefresh: process.env.NODE_ENV !== 'test',
